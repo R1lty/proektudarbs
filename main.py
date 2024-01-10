594a4af05071dd5dc7cf24de7bb5af33d33e7706
Aleksandrs Kunicins <tiv51tiv51@gmail.com>
2024-01-10 22:15:18 +0200

    adding libraries

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import pandas
+
 from classes import *
+
+service = Service()
+option = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=service, options=option)

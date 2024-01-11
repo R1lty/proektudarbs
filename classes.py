8e5792b3ba34edee869e08ed054a547b22ded6d3
Aleksandrs Kunicins <tiv51tiv51@gmail.com>
2024-01-11 11:38:39 +0200

    adding class and data checking

--- classes.py
+++ classes.py
@@ -1,4 +1,18 @@
+
 class Car:
-    year = 2000
-    engine = 1.6
-    transmission = "manual"
+    
+    
+    def write_welcome_message(self):
+        print("""
+            Welcome to the application for finding a suitable car. 
+            We will help you find a car according to your criteria
+            """)
+        
+    def check_yearOfCar(self,yearOfCar):
+        return  True if 1923 < yearOfCar<=2024 else False
+    
+    def check_engine(self,engineOfCar):
+        return True if 0.6 <= engineOfCar <= 7.3 else False
+    
+    def check_transmission(self,carTransmission):
+        return True if carTransmission==("manual" or "automatic") else False
--- main.py
+++ main.py
@@ -7,6 +7,44 @@ import pandas
 
 from classes import *
 
-service = Service()
-option = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=option)
+car = Car()
+
+# service = Service()
+# option = webdriver.ChromeOptions()
+# driver = webdriver.Chrome(service=service, options=option)
+
+#------------------------------------WELCOME MESSAGE-------------------------------------------------------------------------
+
+car.write_welcome_message()
+
+#------------------------------------DATA CHEACKING-------------------------------------------------------------------------
+
+
+while (True):
+    yearOfCar = int(input("Please write what year of car you want to purchase 'e.g 2018' (Year): "))
+    if car.check_yearOfCar(yearOfCar):
+        car.yearOfCar = yearOfCar 
+        break
+    else:
+        print("We cannot find a car of this year, please enter another year")
+
+
+while (True):
+    carEngine = float(input("Now write what engine size you want 'e.g 1.6' (Engine): "))
+    if car.check_engine(carEngine):
+        car.carEngine = carEngine
+        break
+    else:
+        print("We can't find a car with this engine size, please enter another engine")
+
+
+while (True):
+    carTransmission = input("What kind of transmission do you want in your car 'manual/automatic ' (Transmission): ")
+    if car.check_transmission(carTransmission):
+        car.carTransmission = carTransmission
+        break
+    else:
+        print("We can't find a transmission of this type, either manual or automatic")
+
+
+#------------------------------------DATA CHEACKING-------------------------------------------------------------------------
